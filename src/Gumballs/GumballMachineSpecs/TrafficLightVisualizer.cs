7bf54cdc1ac73c3cc682216112155cbe55dd175e
Barry Forrest <barry.forres @gmail.com>
2011-05-09 14:36:13 -0500

    process job png

--- src/Gumballs/GumballMachineSpecs/TrafficLightVisualizer.cs
+++ src/Gumballs/GumballMachineSpecs/TrafficLightVisualizer.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Gumballs;
 using Magnum.StateMachine;
 using Magnum.Visualizers.StateMachine;
@@ -15,8 +16,21 @@ namespace GumballMachineSpecs
             var machine = new TrafficLightMachine();
             var generator = new StateMachineGraphGenerator();
 
-            string filename = Path.Combine(@"C:\Users\Barry Forrest\Documents\Visual Studio 2010\Projects\GumballStateMachine\", string.Format("{0}.png", machine.GetType().Name)); //Path.Combine(Environment.SpecialFolder.ApplicationData.ToString(), "graph.png");
+            string filename = Path.Combine(@"C:\Users\Barry Forrest\Documents\Visual Studio 2010\Projects\GumballStateMachine\", string.Format("{0}.png", machine.GetType().Name)); 
+            //Path.Combine(Environment.SpecialFolder.ApplicationData.ToString(), "graph.png");
             generator.SaveGraphToFile(machine.GetGraphData(), 2560, 1920, filename);
+        }
+
+        [Test]
+        public void strings_are_shared()
+        {
+            var stringArray = new string[] {"String One", "String Two", "String Three"};
+
+            var stringOne = "String One";
+
+            Assert.AreEqual(stringOne, stringArray[0]);
+            Assert.AreSame(stringOne, stringArray[0]);
+
         }
     }
 }
