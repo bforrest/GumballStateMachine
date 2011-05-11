4ba9afeca8c5113f9c6070435d4897754b5418e1
Barry Forrest <barry.forres @gmail.com>
2011-05-11 09:25:54 -0500

    working on a traffic signal

---
+++ src/Gumballs/GumballMachineSpecs/Given_A_New_TrafficLightMachine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gumballs;
+using NUnit.Framework;
+
+namespace GumballMachineSpecs
+{
+    [TestFixture]
+    public class Given_A_New_TrafficLightMachine
+    {
+        private TrafficLightMachine trafficLight;
+        [SetUp]
+        public void TestInitialize()
+        {
+            trafficLight = new TrafficLightMachine();
+        }
+
+        [Test]
+        public void TrafficLightMachine_ShouldInitializeTo_Initial()
+        {
+            Assert.AreEqual(trafficLight.CurrentState, TrafficLightMachine.Initial);
+        }
+
+        [Test]
+        public void LightOperatingState_ShouldBe_Initial()
+        {
+            Assert.IsNotNull(trafficLight.LightState);
+            Assert.AreEqual(trafficLight.LightState.CurrentState, LightOperatingStates.Initial);
+        }
+
+        [Test]
+        public void When_SwitchedOn_StateShouldBe_On()
+        {
+            trafficLight.RaiseEvent(TrafficLightMachine.SwitchedOn);
+            Assert.AreEqual(trafficLight.CurrentState, TrafficLightMachine.On);
+        }
+        [Test]
+        public void When_SwitchedOn_LigthState_ShouldBe_Green()
+        {
+            trafficLight.RaiseEvent(TrafficLightMachine.SwitchedOn);
+            Assert.AreEqual(trafficLight.LightState.CurrentState, LightOperatingStates.Green);
+        }
+    }
+}
---
+++ src/Gumballs/Gumballs/ProcessJobStateMachine.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Magnum.StateMachine;
+
+namespace Gumballs
+{
+    [Serializable]
+    public class ProcessJobStateMachine : StateMachine<ProcessJobStateMachine>
+    {
+        static ProcessJobStateMachine()
+        {
+            //Define();
+        }
+
+        public static State Pooled_Queued { get; set; }
+        public static StateMachine ActiveStates { get; set; }
+
+        public static StateMachine PostActive { get; set; }
+        public static State Completed { get; set; }
+
+
+        
+    }
+
+    [Serializable]
+    public class PostActive : StateMachine<PostActive>
+    {
+        static PostActive()
+        {//
+           // Define();
+        }
+
+        public static State ProcessComplete { get; set; }
+
+        public static State Aborted { get; set; }
+
+        public static State Stopped { get; set; }
+    }
+
+    [Serializable]
+    public class Active : StateMachine<Active>
+    {
+        static Active()
+        {
+            //Define( ()=> ) ;
+            //    );
+        }
+
+        public static State Completed { get; set; }
+    }
+}
